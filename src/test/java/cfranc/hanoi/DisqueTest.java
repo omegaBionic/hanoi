ac52894ebd22daf6db6602065d16157d5e870e41
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-12-09 15:07:39 +0100

    [Test] Added test for disques.

--- src/test/java/cfranc/hanoi/DisqueTest.java
+++ src/test/java/cfranc/hanoi/DisqueTest.java
@@ -5,25 +5,42 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class DisqueTest {
-	
-	Disque dSmall = new Disque(1);
-	Disque dMedium = new Disque(2);
-	Disque dTall = new Disque(3);
 
 	@Test
-	public void testCompareTo() {
-		Disque petit = new Disque(1);
-		Disque moyen = new Disque(2);
-		Disque grand = new Disque(3);
-		assertEquals(-1, petit.compareTo(moyen));
-		assertEquals(-1, moyen.compareTo(grand));
-		//TODO : à compléter
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void compareTo_Small_To_Medium_negative() {
+		Disque dSmall = new Disque(1);
+		Disque dMedium = new Disque(2);
+
+		int expected = -1;
+		int actual = dSmall.compareTo(dMedium);
+		assertEquals(expected, actual);
 	}
 
 	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void compareTo_Medium_To_Small_negative() {
+		Disque dSmall = new Disque(1);
+		Disque dMedium = new Disque(2);
+
+		int expected = 1;
+		int actual = dMedium.compareTo(dSmall);
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
 	public void compareTo_SmallMedium_Negative(){
-		int expected = -1;
-		int actual = dSmall.compareTo(dMedium);
+		Disque dSmall = new Disque(1);
+
+		int expected = 0;
+		int actual = dSmall.compareTo(dSmall);
 		assertEquals(expected, actual);
 	}
 }
