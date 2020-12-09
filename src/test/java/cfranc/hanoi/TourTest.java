d512f811448630346d94cf3d8a16f5882a6f3222
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-12-09 15:07:53 +0100

    [Test] Added test for tour.

--- src/test/java/cfranc/hanoi/TourTest.java
+++ src/test/java/cfranc/hanoi/TourTest.java
@@ -1,14 +1,100 @@
 package cfranc.hanoi;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class TourTest {
 
 	@Test
-	public void test() {
-
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void emplier_TourVide_True() {
+		Tour tour = new Tour(3);
+		boolean condition = tour.estVide();
+		assertTrue(condition);
 	}
 
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void emplier_TourPlein_True() {
+		Tour tour = new Tour(3);
+		Disque d1 = new Disque(1);
+		Disque d2 = new Disque(2);
+		Disque d3 = new Disque(3);
+
+		tour.empiler(d3);
+		tour.empiler(d2);
+		tour.empiler(d1);
+
+		boolean condition = tour.estPleine();
+		assertTrue(condition);
+	}
+
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void estPleine_true() {
+		Tour tour = new Tour(3);
+		Disque d1 = new Disque(1);
+		Disque d2 = new Disque(2);
+		Disque d3 = new Disque(3);
+
+		tour.empiler(d3);
+		tour.empiler(d2);
+		tour.empiler(d1);
+
+		boolean condition = tour.estPleine();
+		assertTrue(condition);
+	}
+
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void estPleine_false() {
+		Tour tour = new Tour(3);
+		Disque d2 = new Disque(2);
+		Disque d3 = new Disque(3);
+
+		tour.empiler(d3);
+		tour.empiler(d2);
+
+		boolean condition = tour.estPleine();
+		assertFalse(condition);
+	}
+
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void diam_DiametreDisqueSommet_Null() {
+		Tour tour = new Tour(3);
+		int expected = Integer.MAX_VALUE;
+		int actual = tour.diam();
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	/**
+	 * GIVEN_WHEN_THEN
+	 */
+	public void diam_DiametreDisqueSommet_NotNull() {
+		Tour tour = new Tour(3);
+		Disque d1 = new Disque(1);
+		Disque d2 = new Disque(2);
+		Disque d3 = new Disque(3);
+
+		tour.empiler(d3);
+		tour.empiler(d2);
+		tour.empiler(d1);
+
+		int expected = 3;
+		int actual = tour.diam();
+		assertEquals(expected, actual);
+	}
 }
